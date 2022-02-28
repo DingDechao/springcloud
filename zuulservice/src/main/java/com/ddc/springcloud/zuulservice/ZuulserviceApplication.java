995cef8dd26747bf4ef89cfabd6771f8bc9ed11f
DingDechao <dingdechao_0486@163.com>
2022-02-28 23:53:46 +0800

    add module zuulservice

---
+++ zuulservice/src/main/java/com/ddc/springcloud/zuulservice/ZuulserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ddc.springcloud.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulserviceApplication.class, args);
+	}
+
+}
